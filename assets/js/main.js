c31e4caa82f6ff1737c54e2b60c3e6751798a3a2
nehabht <ne.bhatti2@icloud.com>
2022-03-28 23:42:39 +0200

    add milestone 2 bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,48 @@
+
+const numbersElement = document.querySelector(".container")
+
 for (let i= 1; i<=100;i++){
     if (i % 3 == 0 && i % 5 == 0){
-      console.log(i,"FizzBuzz")
+        console.log(i,"FizzBuzz");
+        const markupLi = `
+        <div class="number bg_three_five">
+            <div>fizzbuzz</div>
+        </div>
+        `
+        numbersElement.insertAdjacentHTML("beforeend", markupLi)
     } else {
-      if(i % 3 == 0){
-        console.log("Fizz")
-    } if (i % 5 == 0){
-      console.log("Buzz")
-    } if (i % 3 !== 0 && i % 5 !== 0){
-      console.log(i)
+        if(i % 3 == 0){
+            console.log("Fizz");
+            const markupLi = `
+            <div class="number bg_three">
+                <div>fizz</div>
+            </div>
+            `
+            numbersElement.insertAdjacentHTML("beforeend", markupLi)
+            
+    }   if (i % 5 == 0){
+            console.log("buzz");
+            const markupLi = `
+            <div class="number bg_five">
+                <div>buzz</div>
+            </div>
+            `
+            numbersElement.insertAdjacentHTML("beforeend", markupLi)
+           
+            
+    }   if (i % 3 !== 0 && i % 5 !== 0){
+            console.log(i);
+            const markupLi = `
+            <div class="number bg_blue ">
+                <div>${i}</div>
+            </div>
+            `
+            numbersElement.insertAdjacentHTML("beforeend", markupLi)
+            
     }
+        
+    
   }
 }
-    
+
+
